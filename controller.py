1c472324e6dff5e626098c3f901a78186b7d881f
Roy Macdonald <macdonald.roy@gmail.com>
2024-05-24 15:57:51 +0200

    testing timings

--- controller.py
+++ controller.py
@@ -122,8 +122,8 @@ def update():
 
 
 
-# loadpath ="./final_fixture_description_localhost.json"
-loadpath ="./final_fixture_description.json"
+loadpath ="./final_fixture_description_localhost.json"
+# loadpath ="./final_fixture_description.json"
 loadPods(loadpath)
 leds = loadLeds(loadpath)
 strands = loadPods(loadpath)
@@ -135,25 +135,31 @@ loadControllers(loadpath)
 
 # for()
 
-
-chain0 = PodAnimationChain(strands[0])
-chain1 = PodAnimationChain(strands[1])
-chain2 = PodAnimationChain(strands[2])
-chain3 = PodAnimationChain(strands[3])
-chain0.start()
-chain1.start()
-chain2.start()
-chain3.start()
-
-
-
 # for s in strands:
-# 	for p in s:
-# 		p.setOn()
-# 		# p.setOnWhite()
-# 		podAnimations.append(PodAnimation(p));
-# 		podAnimations[len(podAnimations) -1].repeatInfinite()
-# 		podAnimations[len(podAnimations) -1].start()
+# 	podAnimations.append(PodAnimationChain(s));
+# 		# podAnimations[len(podAnimations) -1].repeatInfinite()
+# 	podAnimations[len(podAnimations) -1].start()
+
+
+
+# chain0 = PodAnimationChain(strands[0])
+# chain1 = PodAnimationChain(strands[1])
+# chain2 = PodAnimationChain(strands[2])
+# chain3 = PodAnimationChain(strands[3])
+# chain0.start()
+# chain1.start()
+# chain2.start()
+# chain3.start()
+
+
+
+for s in strands:
+	for p in s:
+		# p.setOn()
+#		# p.setOnWhite()
+		podAnimations.append(PodAnimation(p));
+		podAnimations[len(podAnimations) -1].repeatInfinite()
+		podAnimations[len(podAnimations) -1].start()
 
 # l = chain0.getLast()
 # if l is not None:
