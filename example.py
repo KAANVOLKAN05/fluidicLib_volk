13f5d1d8ea5dfd395f52a12075bb9939094990eb
Roy Macdonald <macdonald.roy@gmail.com>
2024-06-03 01:30:28 +0200

    example

--- example.py
+++ example.py
@@ -12,13 +12,13 @@ atlas = fluidicLib.strands[ATLAS]
 
 
 
-chainLink = ChainLink(atlas[ELECTRON])
-# chainLink = ChainLink(atlas[ELECTRON], 1.0, 3.0, 2.0, 0.5) # with non default blink timings
-chainLink.repeatInfinite()
-chainLink.start()
+# chainLink = ChainLink(atlas[ELECTRON])
+# # chainLink = ChainLink(atlas[ELECTRON], 1.0, 3.0, 2.0, 0.5) # with non default blink timings
+# chainLink.repeatInfinite()
+# chainLink.start()
 
 
-# chain = Chain(([atlas[ELECTRON], atlas[MUON], atlas[TAU]], (atlas[QUARK_DOWN], (atlas[QUARK_UP], atlas[QUARK_DOWN], atlas[PHOTON]), (atlas[W_BOSON], atlas[ELECTRON_NEUTRINO], atlas[MUON_NEUTRINO])), atlas[TAU_NEUTRINO]))
+chain = Chain(([atlas[ELECTRON], atlas[MUON], atlas[TAU]], (atlas[QUARK_DOWN], (atlas[QUARK_UP], atlas[QUARK_DOWN], atlas[PHOTON]), (atlas[W_BOSON], atlas[ELECTRON_NEUTRINO], atlas[MUON_NEUTRINO])), atlas[TAU_NEUTRINO]))
 
 
 # chain = Chain([[atlas[ELECTRON], atlas[MUON], atlas[TAU]], (atlas[QUARK_DOWN], (atlas[QUARK_UP], atlas[QUARK_DOWN], atlas[PHOTON]), (atlas[W_BOSON], atlas[ELECTRON_NEUTRINO], atlas[MUON_NEUTRINO]))])
@@ -32,11 +32,11 @@ chainLink.start()
 # chain = Chain([seqChain, (atlas[QUARK_DOWN], branch0, branch1)])
 
 
-# chain.printStructure()
+chain.printStructure()
 
-# chain.repeatInfinite()
+chain.repeatInfinite()
 
-# chain.start() 
+chain.start() 
 
 
 
