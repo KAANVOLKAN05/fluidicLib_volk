18e6e8989e7fb7b93efbbfb0275604a96b9aa236
Johan S. Bonilla <johan.sebastian.bonilla@cern.ch>
2025-07-25 16:54:25 +0200

    some changes to all_lights_on to allow me to set colors

--- all_lights_on.py
+++ all_lights_on.py
@@ -6,6 +6,8 @@ from FluidicLib import *
 loadpath= "./final_fixture_description.json"
 fluidicLib = FluidicLib(loadpath)
 
+strands = fluidicLib.strands
+
 alice = fluidicLib.strands[0]
 
 atlas = fluidicLib.strands[1]
@@ -16,13 +18,25 @@ lhcb = fluidicLib.strands[3]
 
 #Be happy
 for pod in alice:
-        pod.setOn()
+	pod.color.targetColor.set(250,0,250)
+	pod.setOn()
+	fluidicLib.controllers.updateLedsColors(strands)
+	fluidicLib.controllers.updateArtnet()
 
 for pod in atlas:
-        pod.setOn()
+	pod.color.targetColor.set(0,0,250)
+	pod.setOn()
+	fluidicLib.controllers.updateLedsColors(strands)
+	fluidicLib.controllers.updateArtnet()
 
 for pod in cms:
-        pod.setOn()
+	pod.color.targetColor.set(0,250,0)
+	pod.setOn()
+	fluidicLib.controllers.updateLedsColors(strands)
+	fluidicLib.controllers.updateArtnet()
 
 for pod in lhcb:
+	pod.color.targetColor.set(250,128,13)
 	pod.setOn()
+	fluidicLib.controllers.updateLedsColors(strands)
+	fluidicLib.controllers.updateArtnet()
